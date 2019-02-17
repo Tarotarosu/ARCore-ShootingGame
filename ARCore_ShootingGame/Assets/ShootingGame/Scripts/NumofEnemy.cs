c78af5c68c72e3b7031e65b19e64e0aa55f1b3db
Tarotarosu <shuu.breakin@gmail.com>
2019-02-18 03:21:28 +0900

    新規ファイルの追加

---
+++ ARCore_ShootingGame/Assets/ShootingGame/Scripts/NumofEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumofEnemy : MonoBehaviour {
+
+    public Text noe;
+    public GameObject[] tagObjects;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        tagObjects = GameObject.FindGameObjectsWithTag("enemy");
+        noe.text = "Enemy " + tagObjects.Length + "/5";
+	}
+}
---
+++ ARCore_ShootingGame/Assets/ShootingGame/Scripts/PlayerStatus.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour {
+
+    public int hp;
+    public int maxHP = 10;
+    private PlayerHPScript hpscript;
+    public GameObject gameOver;
+
+
+	// Use this for initialization
+	void Start () {
+        hp = maxHP;
+        hpscript = GameObject.Find("Canvas").GetComponent<PlayerHPScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hp <= 0)
+        {
+            GameOver();
+
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            hp -= 1;
+            hpscript.UpdateHPValue();
+        }
+    }
+
+    public int GetHp()
+    {
+        return hp;
+    }
+    public int GetMaxHp()
+    {
+        return maxHP;
+    }
+
+    public void GameOver()
+    {
+        if (!GameObject.Find("GameOverCanvas(Clone)"))
+        {
+            Instantiate(gameOver);
+        }
+        Invoke("QuitApp", 5);
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+
+}
+
